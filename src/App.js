c0e6b58d2ad0d3f14cbf3f6ff9a1b5cd1877df70
Joe Thornton <joe.clifford.thornton@gmail.com>
2021-08-10 22:35:18 +0100

    Added "Draw Card" button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
-import React, { Component } from 'react';
-import Card from './Card/Card';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./Card/Card";
+import "./App.css";
+import DrawButton from "./DrawButton/DrawButton";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
+    this.updateCard = this.updateCard.bind(this);
+
     this.state = {
       cards: [
-        { id: 1, prompt: "あ", answer_part_1: "a", answer_part_2: "a as in c(a)t" },
-        { id: 2, prompt: "か", answer_part_1: "ka", answer_part_2: "ka as in (ca)ptain" }
+        {
+          id: 1,
+          prompt: "あ",
+          answer_part_1: "a",
+          answer_part_2: "a as in c(a)t",
+        },
+        {
+          id: 2,
+          prompt: "か",
+          answer_part_1: "ka",
+          answer_part_2: "ka as in (ca)ptain",
+        },
       ],
-      currentCard: {
+      currentCard: {},
+    };
+  }
 
-      }
-    }
+  componentWillMount() {
+    const currentCards = this.state.cards;
 
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards),
+    });
+  }
+
+  getRandomCard(cards) {
+    return cards[Math.floor(Math.random() * cards.length)];
+  }
+
+  updateCard() {
+    console.log("New Card");
   }
 
   render() {
     return (
       <div className="App">
-        <Card />
+        <div className="cardRow">
+          <Card
+            prompt={this.state.currentCard.prompt}
+            answer_part_1={this.state.currentCard.answer_part_1}
+            answer_part_2={this.state.currentCard.answer_part_2}
+          />
+        </div>
+        <div className="buttonRow">
+          <DrawButton drawCard={this.updateCard} />
+        </div>
       </div>
-    )
-  };
+    );
+  }
 }
 
 export default App;
--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
 const Card = (props) => (
-    <div className="card-container">
-        <div className="card">
-            <div className="front">
-                <div className="prompt-text">
-                    Prompt
-                </div>
-            </div>
+  <div className="card-container">
+    <div className="card">
+      <div className="front">
+        <div className="prompt-text">{props.prompt}</div>
+      </div>
 
-            <div className="back">
-                <div className="answer-part1">
-                    Answer Part 1
-                </div>
-                <div className="answer-part2">
-                    Answer Part 2
-                </div>
-            </div>
-        </div>
+      <div className="back">
+        <div className="answer-part1">{props.answer_part_1}</div>
+        <div className="answer-part2">{props.answer_part_2}</div>
+      </div>
     </div>
-)
+  </div>
+);
 
-export default Card
+export default Card;
