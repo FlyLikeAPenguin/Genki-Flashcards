8daf888e3dae504d5aea830d46fd0c9988302017
Joe Thornton <joe.clifford.thornton@gmail.com>
2021-08-10 21:59:18 +0100

    Added temp card data; displayed a dummy card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+import React, { Component } from 'react';
+import Card from './Card/Card';
 import './App.css';
+import { IsConstructor } from 'es-abstract';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cards: [
+        {id:1, prompt:"あ", answer_part_1:"a", answer_part_2:"a as in c(a)t"},
+        {id:2, prompt:"か", answer_part_1:"ka", answer_part_2:"ka as in (ca)ptain"}
+      ],
+      currentCard: {
+
+      }
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Card />
+      </div>
+    )
+  };
 }
 
 export default App;
---
+++ src/Card/Card.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+const Card = (props) => (
+    <div className="card-container">
+        <div className="card">
+            <div className="front">
+                <div className="prompt-text">
+                    Prompt
+                </div>
+            </div>
+
+            <div className="back">
+                <div className="answer-part1">
+                    Answer Part 1
+                </div>
+                <div className="answer-part2">
+                    Answer Part 2
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Card
