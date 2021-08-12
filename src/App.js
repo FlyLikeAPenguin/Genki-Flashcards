a2c2a3e4d9ae114f05fcf731a757a8d3491fba0f
Joe Thornton <joe.clifford.thornton@gmail.com>
2021-08-12 20:14:36 +0100

    Added firebase connection

--- src/App.js
+++ src/App.js
@@ -2,47 +2,62 @@ import React, { Component } from "react";
 import Card from "./Card/Card";
 import "./App.css";
 import DrawButton from "./DrawButton/DrawButton";
+import firebase from "firebase/app";
+import "firebase/database";
+import { DB_CONFIG } from "./Config/Firebase/db_config";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    if (firebase.apps.length === 0) {
+      firebase.initializeApp(DB_CONFIG);
+    }
+    this.database = firebase.database().ref().child("cards");
     this.updateCard = this.updateCard.bind(this);
 
     this.state = {
-      cards: [
-        {
-          id: 1,
-          prompt: "あ",
-          answer_part_1: "a",
-          answer_part_2: "a as in c(a)t",
-        },
-        {
-          id: 2,
-          prompt: "か",
-          answer_part_1: "ka",
-          answer_part_2: "ka as in (ca)ptain",
-        },
-      ],
+      cards: [],
       currentCard: {},
     };
   }
 
   componentWillMount() {
+    console.log(firebase.database().ref().child("cards"));
+
     const currentCards = this.state.cards;
 
-    this.setState({
-      cards: currentCards,
-      currentCard: this.getRandomCard(currentCards),
+    this.database.on("child_added", (snap) => {
+      currentCards.push({
+        id: snap.key,
+        prompt: snap.val().prompt,
+        answer_part_1: snap.val().answer_part_1,
+        answer_part_2: snap.val().answer_part_2,
+      });
+
+      this.setState({
+        cards: currentCards,
+        currentCard: this.getRandomCard(currentCards),
+      });
     });
   }
 
   getRandomCard(cards) {
-    return cards[Math.floor(Math.random() * cards.length)];
+    var randomIndex = Math.floor(Math.random() * cards.length);
+    var card = cards[randomIndex];
+    if (card === this.state.currentCard) {
+      this.getRandomCard(cards);
+    }
+
+    return card;
   }
 
   updateCard() {
-    console.log("New Card");
+    const currentCards = this.state.cards;
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards),
+    });
   }
 
   render() {
