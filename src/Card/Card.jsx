a815abddbb9a157a69c29e2af306de4bf3defd49
Joe Thornton <joe.clifford.thornton@gmail.com>
2021-08-15 10:19:31 +0100

    Updated stying - layout/shadow

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./Card.css";
 
 const Card = (props) => (
-  <div className="card-container">
-    <div className="card">
-      <div className="front">
+  <div className="card">
+    <div className="card-inner">
+      <div className="card-front">
         <div className="kanji">{props.Kanji}</div>
       </div>
 
-      <div className="back">
+      <div className="card-back">
         <div className="reading">{props.Reading}</div>
         <div className="definition">{props.Definition}</div>
       </div>
