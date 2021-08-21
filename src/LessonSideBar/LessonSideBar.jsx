607b1d23c517220f1ac23ae47be2e77119b1820f
Joe Thornton <joe.clifford.thornton@gmail.com>
2021-08-21 23:04:34 +0100

    More CSS adjustments

--- src/LessonSideBar/LessonSideBar.jsx
+++ src/LessonSideBar/LessonSideBar.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import "./LessonSideBar.css";
 
 const LessonSideBar = (props) => (
-  <ul class="side-lessons">
-    <li className="lesson-list-item">
-      {props.activeLessons.length === 0 ? "All" : "None"}
-    </li>
-    {props.lessons.map((lesson) => (
-      <li className="lesson-list-item">Lesson {lesson.number}</li>
-    ))}
-  </ul>
+  <div>
+    <ul class="side-lessons">
+      <li className="lesson-list-item">
+        {props.activeLessons.length === 0 ? "All" : "None"}
+      </li>
+      {props.lessons.map((lesson) => (
+        <li className="lesson-list-item">Lesson {lesson.number}</li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default LessonSideBar;
