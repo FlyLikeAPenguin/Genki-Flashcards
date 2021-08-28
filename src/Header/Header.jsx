ffbad9c7a032382df0f9fe99057c6ec44e3260ee
Joe Thornton <joe.clifford.thornton@gmail.com>
2021-08-28 19:31:01 +0100

    Added Ko-fi link

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AiFillGithub } from "react-icons/ai";
+import { BiDonateHeart } from "react-icons/bi";
 import "./Header.css";
 
 const Header = () => (
@@ -7,6 +8,14 @@ const Header = () => (
     <ul>
       <li className="topnav-bold">Kanji/Kana Flashcards</li>
       <li className="topnav-right">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://ko-fi.com/flylikeapenguin"
+          className="topnav-light">
+          Send a Thank You &nbsp;
+          <BiDonateHeart className="icon" />
+        </a>
         <a
           target="_blank"
           rel="noreferrer"
